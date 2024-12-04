87d38eccce9f7ab2920b41b0569254fa2d17116e
ICrazyDarkI <cristianyanez999@gmail.com>
2024-12-04 01:51:30 +0100

    Initial Home Templates and Controllers

---
+++ SmartPlay/SmartPlay/src/main/java/com/tfg/SmartPlay/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.tfg.SmartPlay.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "home";    
+    }
+}
