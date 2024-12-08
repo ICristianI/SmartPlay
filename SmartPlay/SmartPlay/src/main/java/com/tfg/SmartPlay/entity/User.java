0fcf98626aee57080bcd28552aaa250ff382164f
ICrazyDarkI <cristianyanez999@gmail.com>
2024-12-08 15:21:18 +0100

    ProfileUser Basic

--- SmartPlay/SmartPlay/src/main/java/com/tfg/SmartPlay/entity/User.java
+++ SmartPlay/SmartPlay/src/main/java/com/tfg/SmartPlay/entity/User.java
@@ -23,16 +23,16 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String nombre;
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = false)
     private Integer edad;
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false,unique = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
